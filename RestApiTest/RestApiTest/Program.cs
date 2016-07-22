d4fccf25ed037d8935b86184444e2593be7f59c3
Terry Trent <terry.trent@gmail.com>
2016-07-22 18:41:05 -0400

    added utility

---
+++ RestApiTest/RestApiTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+public enum HttpVerb
+{
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
+
+namespace RestApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var url = "http://api.openweathermap.org/data/2.1/find/city?lat=39.0438&lon=77.4874&cnt=10";
+
+            var syncClient = new WebClient();
+            var content = syncClient.DownloadString(url);
+
+        }
+    }
+}
---
+++ SqlLiteTest/Utility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLiteTest
+{
+    public class Utility
+    {
+        public bool checkIsvalid(string comparison, string pattern)
+        {
+            Regex comparisonRegex = new Regex(pattern);
+            bool match = comparisonRegex.IsMatch(comparison);
+            return match;
+        }
+
+        public void notValidThrowException(string objectToBeValidated, string message, bool isObjectValidated, string nameOfObjectToBeValidated)
+        {
+            if (!isObjectValidated)
+            {
+                throw new System.Exception($"{nameOfObjectToBeValidated} {message}: {objectToBeValidated}");
+            }
+        }
+
+        public List<string> generateFileAndDirectoryInfo(string projectName,string dbName)
+        {
+            string filePath = (Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).ToString() + $"\\{projectName}";
+            string fullPath = $"{filePath}\\{dbName}";
+
+            List<string> dataToReturn = new List<string>();
+            dataToReturn.Add(filePath);
+            dataToReturn.Add(dbName);
+            dataToReturn.Add(fullPath);
+
+            return dataToReturn;
+        }
+    }
+}
