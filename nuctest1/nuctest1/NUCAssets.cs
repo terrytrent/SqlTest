6eb7a3f0b6c62063ff6f815349bad4ae7ad1a4e5
Terry Trent <ttrent1101@gmail.com>
2016-07-30 21:11:51 -0400

    nucasset - added sql tests

--- nuctest1/nuctest1/NUCAssets.cs
+++ nuctest1/nuctest1/NUCAssets.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Threading;
+using System.Data.SqlClient;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- nuctest1/nuctest1/Program.cs
+++ nuctest1/nuctest1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,10 +16,35 @@ namespace nuctest1
             //Comp1.getInfo();
             //Comp1.storeInfoInDB();
 
-            NUCAssetStorage HD1 = new NUCAssetStorage();
-            HD1.setInfo("654321", "Seagate", Statuses.Available, AssetTypes.HD, 256);
-            HD1.getInfo();
-            HD1.storeInfoInDB();
+            //NUCAssetStorage HD1 = new NUCAssetStorage();
+            //HD1.setInfo("654321", "Seagate", Statuses.Available, AssetTypes.HD, 256);
+            //HD1.getInfo();
+            //HD1.storeInfoInDB();
+
+            SqlConnection myConnection = new SqlConnection("user id=ttrent;" +
+                                       "password=;server=localhost;" +
+                                       "Trusted_Connection=yes;" + "database=test;");
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            //string command = "CREATE DATABASE test";
+            //SqlCommand sqlCommand = new SqlCommand(command, myConnection);
+            //sqlCommand.ExecuteNonQuery();
+
+            //string command = "CREATE TABLE test2 (ID int PRIMARY KEY NOT NULL, Name varchar(25) NOT NULL)";
+            //SqlCommand sqlCommand = new SqlCommand(command, myConnection);
+            //sqlCommand.ExecuteNonQuery();
+
+            string command = "INSERT INTO test2 (ID,Name) VALUES (1,'ok')";
+            SqlCommand sqlCommand = new SqlCommand(command, myConnection);
+            sqlCommand.ExecuteNonQuery();
+
         }
     }
 }
